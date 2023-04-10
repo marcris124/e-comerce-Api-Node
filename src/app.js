9816a8da7e7ad7acaa34fee911bfac3888b547a1
marcris124 <martinavendano095@gmail.com>
2023-04-10 11:08:31 -0500

    app config

--- src/app.js
+++ src/app.js
@@ -1,14 +1,12 @@
 const express = require("express");
 const ApiRoutes = require("./routes");
 require("dotenv").config();
-const swaggerUi = require("swagger-ui-express");
-const swaggerDoc = require("./swagger.json")
 const PORT = process.env.PORT;
 const app = express();
 app.use(express.json());
 
 ApiRoutes(app);
-app.use("/api/v1/docs", swaggerUi.serve, swaggerUi.setup(swaggerDoc))
+
 app.use((error, req, res, next) => res.status(400).json(error));
 
 app.get("/", (req, res) => {
--- src/routes/index.js
+++ src/routes/index.js
@@ -3,13 +3,16 @@ const authRoutes = require("./auth.routes");
 const productRoutes = require("./product.routes");
 const productsInCarRoutes = require("./productsInCar.routes");
 const Orderouter = require("./order.routes");
+const swaggerUi = require("swagger-ui-express");
+const swaggerDoc = require("../swagger.json")
 
 const ApiRoutes = (app) => {
   app.use("/api/v1", userRoutes);
   app.use("/api/v1", authRoutes);
   app.use("/api/v1", productRoutes);
   app.use("/api/v1", productsInCarRoutes);
-  app.use("/api/v1", Orderouter)
+  app.use("/api/v1", Orderouter);
+  app.use("/api/v1/docs", swaggerUi.serve, swaggerUi.setup(swaggerDoc))
 };
 
 module.exports = ApiRoutes;
